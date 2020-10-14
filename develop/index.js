77a820540303182116d45ad00de9936814e73723
maxonemillion <max.daniel@utah.edu>
2020-10-14 17:27:57 -0600

    initial commit

---
+++ develop/index.js
@@ -0,0 +1,35 @@
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const console = require('console.table');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "employees_db"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected");
+    runInquiry(); 
+});
+
+function runInquiry() {
+    inquirer.prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "View departments",
+            "View roles",
+            "View employees",
+            "Add department",
+            "Add role",
+            "Add employee",
+            "Update role",
+            "Exit",
+        ]
+    })
+}
